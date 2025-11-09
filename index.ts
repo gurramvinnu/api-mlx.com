d22df3d59455785139eb15d9a831a41cf9975e7a
kjacobpaul490 <jacobpaul.kuchipudi@gmail.com>
2025-11-09 17:56:25 +0530

    feat: get paitent by guid

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mathRoutes from './src/routes/mathRoutes.js';
 import ordersRoutes from './src/routes/ordersRoute.js';
+import patientRoutes from './src/routes/patientRoutes.js';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const port = 3000;
 app.use(express.json());
 app.use("/",mathRoutes);
 app.use("/orders",ordersRoutes);
+
+app.use("/patients", patientRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
